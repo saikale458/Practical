a970a4fcd840ebe2202be3bceeac167f5c376472
Sameer Gangurde <165358832+sameergangurde71@users.noreply.github.com>
2025-11-06 00:49:59 +0530

    changes

--- BT/BT-2/bank.sol
+++ BT/BT-2/bank.sol
@@ -1,36 +1,28 @@
-//SPDX-License-Identifier: MIT 
-
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-//i created a smart contract that allows a user to deposit, withdraw and save ETH!!
-
 contract Bank{
-    //we mapped the address of the caller balance in the contract
-    mapping(address => uint) public balances;
-
-// whatever the user deposit is added to msg.value of the sender address we mapped above
-    function deposit() public payable{
-        balances[msg.sender] += msg.value; 
-    }
-    
-//we create the fucntion of witdraw 
-    function withdraw(uint _amount) public{
-        //we create a require arg to make sure the balance of the sender is >= _amount if not ERR
-        require(balances[msg.sender]>= _amount, "Not enough ether");
-        //if the amount is availabe we subtract it from the sender 
-        balances[msg.sender] -= _amount;
-        //True bool is called to confirm the amount
-        (bool sent,) = msg.sender.call{value: _amount}("Sent");
-        require(sent, "failed to send ETH");
-
-        
+    address private accOwner;
+    uint256 balance=0;
+    constructor(){
+        accOwner=msg.sender;
     }
 
-    function getBal() public view returns(uint){
-        return address(this).balance;
+    function Deposit() public payable{
+        require(accOwner==msg.sender,"You are not an account owner!!");
+        require(msg.value > 0, "Amount should be greater than 0.");
+        balance+=msg.value;
     }
 
+    function Withdraw() public payable {
+        require(accOwner==msg.sender,"You are not an account owner");
+        require(msg.value > balance,"Account doesnot have sufficient balance!");
+        balance-=msg.value;
+    }
 
-
-
+    function showBalance() public view returns(uint256){
+        require(accOwner==msg.sender,"You are not an account owner");
+        return balance;
+    }
 }
+
